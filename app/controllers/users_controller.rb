7601576956178933e032d715b2841094f0e93930
saley Maiga <saleymaiga@gmail.com>
2014-03-26 18:17:37 -0400

    Added form for user update and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,50 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
+
   def new
   end
+
 
   def show
   	@user = User.find(params[:id])
   end
 
+
   def destroy
   	@user = User.find(params[:id])
   	if @user.destroy
-  		flash[:notice] = 
-  	
+  		flash[:notice] = "Your account was deleted successfully."
+  	else
+  	    flash[:alert] = "Oups! There was a problem deleting your account."
+  	end
+  	redirect_to "/users"
   end
 
+
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.where(id: params[:id]).first
+  	if @user.update_attributes(params[:user])
+  		flash[:notice] = "Your account was updated successfully"
+  	else
+  	    flash[:notice] = "There was a problem updating your account"	
+
+  	
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
